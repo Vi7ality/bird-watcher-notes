53c06e68052f4810130f5f9a0631cecafebc959f
Vitalii S. <svitalii138@gmail.com>
2024-12-11 15:04:12 +0200

    Observation home page added

--- src/pages/Observations.tsx
+++ src/pages/Observations.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { getObservations } from "../utils/localStorageManage";
+import { Link } from "react-router-dom";
+
 const Observations = () => {
-  return <></>;
+  const [observations, setObservations] = useState(getObservations());
+
+  useEffect(() => {
+    setObservations(getObservations());
+  }, []);
+
+  return (
+    <div>
+      <h1>Observation list</h1>
+      <ul>
+        {observations.map((obs) => (
+          <li key={obs.id}>
+            {obs.date} {obs.time} — {obs.bird} в {obs.location}{" "}
+            <Link to={`/edit/${obs.id}`}>Edit</Link> | <Link to={`/delete/${obs.id}`}>Delete</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/add">Add new observation</Link>
+    </div>
+  );
 };
 
 export default Observations;
